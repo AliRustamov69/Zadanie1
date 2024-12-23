15bc5cb11f9a7314e37773ea0debe314eeff0bca
AliRustamov69 <alirus379@gmail.com>
2024-12-23 13:51:24 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        List<int> a = new List<int> { -1, 2, -3, 4, 5, 6, 12 };
-
-        // Сумма положительных элементов
-        int sumPositive = a.Where(x => x > 0).Sum();
-        Console.WriteLine("Сумма положительных элементов: " + sumPositive);
-
-        // Сумма элементов, которые больше единицы и меньше десяти
-        int sumBetweenOneAndTen = a.Where(x => x > 1 && x < 10).Sum();
-        Console.WriteLine("Сумма элементов больше единицы и меньше десяти: " + sumBetweenOneAndTen);
-
-        // Сумма всех элементов
-        int totalSum = a.Sum();
-        Console.WriteLine("Сумма всех элементов: " + totalSum);
+        // Внешний цикл для строк таблицы
+        for (int i = 1; i <= 9; i++)
+        {
+            // Внутренний цикл для столбцов таблицы
+            for (int j = 1; j <= 9; j++)
+            {
+                // Выводим результат умножения в нужном формате
+                Console.Write($"{i} x {j} = {i * j}\t");
+            }
+            // Переход на новую строку после завершения внутреннего цикла
+            Console.WriteLine();
+        }
     }
 }
